a98a9301d3c3387f9678e3b8cb48e7343ff58e18
alexander <alvadpash@yandex.ru>
2018-10-10 02:16:44 +0300

    hw_1

--- src/storage/ThreadSafeSimpleLRU.h
+++ src/storage/ThreadSafeSimpleLRU.h
@@ -8,53 +8,53 @@
 #include "SimpleLRU.h"
 
 namespace Afina {
-namespace Backend {
+    namespace Backend {
 
 /**
  * # SimpleLRU thread safe version
  *
  *
  */
-class ThreadSafeSimplLRU : public SimpleLRU {
-public:
-    ThreadSafeSimplLRU(size_t max_size = 1024) : SimpleLRU(max_size) {}
-    ~ThreadSafeSimplLRU() {}
+        class ThreadSafeSimplLRU : public SimpleLRU {
+        public:
+            ThreadSafeSimplLRU(size_t max_size = 1024) : SimpleLRU(max_size) {}
+            ~ThreadSafeSimplLRU() {}
 
-    // see SimpleLRU.h
-    bool Put(const std::string &key, const std::string &value) override {
-        // TODO: sinchronization
-        return SimpleLRU::Put(key, value);
-    }
+            // see SimpleLRU.h
+            bool Put(const std::string &key, const std::string &value) override {
+                std::lock_guard<std::mutex> lock(_mutex);
+                return SimpleLRU::Put(key, value);
+            }
 
-    // see SimpleLRU.h
-    bool PutIfAbsent(const std::string &key, const std::string &value) override {
-        // TODO: sinchronization
-        return SimpleLRU::PutIfAbsent(key, value);
-    }
+            // see SimpleLRU.h
+            bool PutIfAbsent(const std::string &key, const std::string &value) override {
+                std::lock_guard<std::mutex> lock(_mutex);
+                return SimpleLRU::PutIfAbsent(key, value);
+            }
 
-    // see SimpleLRU.h
-    bool Set(const std::string &key, const std::string &value) override {
-        // TODO: sinchronization
-        return SimpleLRU::Set(key, value);
-    }
+            // see SimpleLRU.h
+            bool Set(const std::string &key, const std::string &value) override {
+                std::lock_guard<std::mutex> lock(_mutex);
+                return SimpleLRU::Set(key, value);
+            }
 
-    // see SimpleLRU.h
-    bool Delete(const std::string &key) override {
-        // TODO: sinchronization
-        return SimpleLRU::Delete(key);
-    }
+            // see SimpleLRU.h
+            bool Delete(const std::string &key) override {
+                std::lock_guard<std::mutex> lock(_mutex);
+                return SimpleLRU::Delete(key);
+            }
 
-    // see SimpleLRU.h
-    bool Get(const std::string &key, std::string &value) const override {
-        // TODO: sinchronization
-        return SimpleLRU::Get(key, value);
-    }
+            // see SimpleLRU.h
+            bool Get(const std::string &key, std::string &value) override {
+                std::lock_guard<std::mutex> lock(_mutex);
+                return SimpleLRU::Get(key, value);
+            }
 
-private:
-    // TODO: sinchronization primitives
-};
+        private:
+            mutable std::mutex _mutex;
+        };
 
-} // namespace Backend
+    } // namespace Backend
 } // namespace Afina
 
 #endif // AFINA_STORAGE_THREAD_SAFE_SIMPLE_LRU_H
